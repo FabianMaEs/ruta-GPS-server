c48e0005b3de88c03cddbd4ad1a8fd6b66ca8005
Fabián Macías Esparza <79736165+FabianMaEs@users.noreply.github.com>
2024-06-17 19:01:38 -0600

    Update server.js

--- server.js
+++ server.js
@@ -1,10 +1,9 @@
-const axios = require('axios');
-const https = require('https');
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const fs = require('fs');
 const moment = require('moment-timezone');
+const { log } = require('console');
 const app = express();
 
 let coordinates = [];
@@ -12,141 +11,87 @@ let coordinates = [];
 app.use(cors());
 app.use(bodyParser.json());
 app.use(express.static('public'));
-app.use((req, res, next) => {
-  res.setHeader('Permissions-Policy', 'interest-cohort=()'); // Asegúrate de que esté bien configurado según tu necesidad y las especificaciones actuales.
-  next();
-});
 
 function getMexicoTime() {
-  return moment.tz("America/Mexico_City").format('YYYY-MM-DD HH:mm:ss');
+return moment.tz("America/Mexico_City").format('YYYY-MM-DD HH:mm
+');
 }
 
 function logWithTimestamp(message) {
-  const timestamp = getMexicoTime();
-  const logMessage = `[${timestamp}] ${message}\n`;
-  console.log(logMessage);
+const timestamp = getMexicoTime();
+const logMessage = [${timestamp}] ${message}\n;
+console.log(logMessage);
 
-  fs.appendFile('log.txt', logMessage, (err) => {
-    if (err) {
-      console.error('Error guardando el log en el archivo:', err);
-    }
-  });
+fs.appendFile('log.txt', logMessage, (err) => {
+if (err) {
+console.error('Error guardando el log en el archivo:', err);
+}
+});
 }
 
 function guardarCoordenadas() {
-  fs.writeFile('coordenadas.json', JSON.stringify(coordinates), err => {
-    if (err) {
-      logWithTimestamp('Error guardando coordenadas: ' + err);
-    } else {
-      logWithTimestamp('Coordenadas guardadas en el archivo.');
-    }
-  });
+fs.writeFile('coordenadas.json', JSON.stringify(coordinates), err => {
+if (err) {
+logWithTimestamp('Error guardando coordenadas: ' + err);
+} else {
+logWithTimestamp('Coordenadas guardadas en el archivo.');
+}
+});
 }
 
 fs.readFile('coordenadas.json', (err, data) => {
-  if (err) {
-    logWithTimestamp('Error leyendo el archivo de coordenadas: ' + err);
-    return;
-  }
-  coordinates = JSON.parse(data);
-  logWithTimestamp('Coordenadas cargadas desde el archivo: ' + JSON.stringify(coordinates));
-});
-
-// Configuración para usar el proxy de Fixie con Axios
-const fixieUrl = process.env.FIXIE_URL;
-if (!fixieUrl) {
-  console.error('La variable de entorno FIXIE_URL no está configurada.');
-  process.exit(1); // Salir del proceso con un código de error
+if (err) {
+logWithTimestamp('Error leyendo el archivo de coordenadas: ' + err);
+return;
 }
-
-// Extraer host, puerto y credenciales del FIXIE_URL
-const fixieParts = fixieUrl.match(/^https?:\/\/([^:]+):([^@]+)@([^:]+):(\d+)/);
-if (!fixieParts) {
-  console.error('Formato de FIXIE_URL no válido.');
-  process.exit(1); // Salir del proceso con un código de error
-}
-
-// Configurar una instancia de Axios con agentes HTTPS personalizados
-const agent = new https.Agent({
-  rejectUnauthorized: false, // Solo para pruebas, configúralo correctamente para producción
-  secureProtocol: 'TLSv1_3', // Usa 'TLSv1_3_method' si prefieres TLS 1.3
-});
-
-
-const axiosInstance = axios.create({
-  httpsAgent: agent,
-  proxy: {
-    host: fixieParts[3],
-    port: parseInt(fixieParts[4]),
-    auth: {
-      username: fixieParts[1],
-      password: fixieParts[2]
-    }
-  }
+coordinates = JSON.parse(data);
+logWithTimestamp('Coordenadas cargadas desde el archivo: ' + JSON.stringify(coordinates));
 });
 
 app.post('/api/coordinates', (req, res) => {
-  logWithTimestamp('Petición POST req.body: ' + JSON.stringify(req.body));
-  const { latitude, longitude } = req.body;
-  if (typeof latitude !== 'number' || typeof longitude !== 'number') {
-    return res.status(400).send({ message: 'Invalid coordinates format' });
-  }
+logWithTimestamp('Petición POST: ' + req.body);
+const { latitude, longitude } = req.body;
+if (typeof latitude !== 'number' || typeof longitude !== 'number') {
+return res.status(400).send({ message: 'Invalid coordinates format' });
+}
 
-  axiosInstance.post('https://proyecto-electronica-34053442d1e0.herokuapp.com/api/coordinates', { latitude, longitude })
-  .then(response => {
-    logWithTimestamp('Respuesta del servidor: ' + JSON.stringify(response.data));
-    coordinates.push({ latitude, longitude });
-    logWithTimestamp('Coordenadas recibidas: ' + JSON.stringify({ latitude, longitude }));
-    guardarCoordenadas();
-    res.status(200).send({ message: 'Coordinates saved' });
-  })
-  .catch(error => {
-    if (error.response) {
-      // La solicitud fue hecha y el servidor respondió con un código de estado que no está en el rango de 2xx
-      logWithTimestamp('Error en la solicitud POST: ' + error.response.status + ' ' + error.response.statusText);
-      res.status(500).send({ message: 'Error saving coordinates' });
-    } else if (error.request) {
-      // La solicitud fue hecha pero no se recibió respuesta
-      logWithTimestamp('No se recibió respuesta del servidor');
-      res.status(500).send({ message: 'No response from server' });
-    } else {
-      // Error configurando la solicitud
-      logWithTimestamp('Error configurando la solicitud: ' + error.message);
-      res.status(500).send({ message: 'Error setting up request' });
-    }
-    logWithTimestamp('Error completo: ' + error);
-  });
-
+coordinates.push({ latitude, longitude });
+logWithTimestamp('Coordenadas recibidas: ' + JSON.stringify({ latitude, longitude }));
+guardarCoordenadas();
+res.status(200).send({ message: 'Coordinates saved' });
 });
 
 app.get('/api/coordinates', (req, res) => {
-  if (coordinates.length === 0) {
-    logWithTimestamp('No hay coordenadas disponibles');
-    return res.status(404).send({ message: 'Coordinates not found' });
-  }
-  res.status(200).send(coordinates);
+if (coordinates.length === 0) {
+logWithTimestamp('No hay coordenadas disponibles');
+return res.status(404).send({ message: 'Coordinates not found' });
+}
+//logWithTimestamp('Coordenadas enviadas: ' + JSON.stringify(coordinates));
+res.status(200).send(coordinates);
 });
 
 app.delete('/api/coordinates', (req, res) => {
-  coordinates = [];
-  logWithTimestamp('Coordenadas borradas');
-  guardarCoordenadas();
-  res.status(200).send({ message: 'Coordinates deleted' });
-  logWithTimestamp('--------- Todas las coordenadas han sido borradas ---------');
+coordinates = [];
+logWithTimestamp('Coordenadas borradas');
+guardarCoordenadas();
+res.status(200).send({ message: 'Coordinates deleted' });
+// Escribir en log.txt
+logWithTimestamp('--------- Todas las coordenadas han sido borradas ---------');
 });
 
 app.get('/log', (req, res) => {
-  fs.readFile('log.txt', 'utf8', (err, data) => {
-    if (err) {
-      logWithTimestamp('Error leyendo el archivo de log: ' + err);
-      return res.status(500).send({ message: 'Error reading log file' });
-    }
-    const formattedData = data.replace(/\n/g, '<br>');
-    res.status(200).send(formattedData);
-  });
+fs.readFile('log.txt', 'utf8', (err, data) => {
+if (err) {
+logWithTimestamp('Error leyendo el archivo de log: ' + err);
+return res.status(500).send({ message: 'Error reading log file' });
+}
+// Reemplazar saltos de línea por <br> tags
+const formattedData = data.replace(/\n/g, '<br>');
+res.status(200).send(formattedData);
+});
 });
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  logWithTimestamp(`Server is running on port ${PORT}`);
+logWithTimestamp(Server is running on port ${PORT});
 });
